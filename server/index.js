7d27a79844dac2a5ba416a45dc369dbf00204ac2
Javlonbek <javlonarjei042@gmail.com>
2019-10-25 19:22:57 +0500

    server static

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ var app=express();
 
 app.use(express.static(path.join(__dirname,'..','client','build')));
 
-// app.use(function(req,res,next){
-//   // if(app.get('env')==='development'){
-//   //   res.send('default response');
-//   // }else{
-//     console.log('env',app.get('env'));
-//     res.sendFile(path.join(__dirname,'..','client','build','index.html'));
-//   //}
-// });
+app.use(function(req,res,next){
+  if(app.get('env')==='development'){
+    res.send('default response');
+  }else{
+    console.log('env',app.get('env'));
+    res.sendFile(path.join(__dirname,'..','client','build','index.html'));
+  }
+});
 
 const port=process.env.PORT||3001;
 app.listen(port);
