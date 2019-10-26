29dd2445ad098239d083a08a4ae3855cb89e59dc
Javlonbek <javlonarjei042@gmail.com>
2019-10-26 14:16:01 +0500

    build static files

---
+++ client/build/precache-manifest.18a3013688e75f410301c1a72e7b9466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d32e178f7ec92a71c869c316976054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26855915522db3ac384b",
+    "url": "/static/css/main.cff4e0a5.chunk.css"
+  },
+  {
+    "revision": "777d13e322b79508cf9c",
+    "url": "/static/js/2.40d9e9fd.chunk.js"
+  },
+  {
+    "revision": "26855915522db3ac384b",
+    "url": "/static/js/main.393b33f3.chunk.js"
+  },
+  {
+    "revision": "a7927b55037a4ea9d056",
+    "url": "/static/js/runtime-main.b2df4a99.js"
+  }
+]);
